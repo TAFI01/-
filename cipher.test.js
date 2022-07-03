92a27ab195bb87823f2118993f84ddc06e29d4dd
TAFI01 <108628226+TAFI01@users.noreply.github.com>
2022-07-03 21:28:18 +0300

    Delete cipher.test.js

--- cipher.test.js
+++
@@ -1,9 +0,0 @@
-const { encrypt, decrypt } = require("./index");
-
-test("encrypt test", () => {
-  expect(encrypt("abc")).toEqual("abc");
-});
-
-test("decrypt test", () => {
-  expect(decrypt("abc")).toEqual("abc");
-});
